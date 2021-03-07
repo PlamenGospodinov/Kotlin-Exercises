da62da66e7519ea793d3dac966408317c662be76
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-07 23:51:57 +0200

    Add files via upload

---
+++ Operators.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+
+    val number1 = 12.5
+    val number2 = 3.5
+    var result: Double
+
+    result = number1 + number2
+    println("number1 + number2 = $result")
+
+    result = number1 - number2
+    println("number1 - number2 = $result")
+
+    result = number1 * number2
+    println("number1 * number2 = $result")
+
+    result = number1 / number2
+    println("number1 / number2 = $result")
+
+    result = number1 % number2
+    println("number1 % number2 = $result")
+
+    val start = "Talk is cheap. "
+    val middle = "Show me the code. "
+    val end = "- Linus Torvalds"
+
+    val resultt = start + middle + end
+    println(resultt)
+}
