e19fc55de7a7d9e5b6e81b2cd3b61d6dd5105ef9
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-10 22:34:26 +0200

    Add files via upload

---
+++ TypeConversion.kt
@@ -0,0 +1,10 @@
+fun main(args : Array<String>) {
+    val number1: Int = 545344
+    val number2: Byte = number1.toByte()
+    val number3: Int = 55
+    val number4: Long = number1.toLong()
+    println("number1 = $number1")
+    println("number2 = $number2")
+    println("number3 = $number3")
+    println("number4 = $number4")
+}
