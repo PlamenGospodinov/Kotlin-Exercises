d5fde375510469cb41206f7f345209838820e3d4
TheWolverine <30501186+TheWolverineCoder@users.noreply.github.com>
2021-03-13 13:41:09 +0200

    Add files via upload

---
+++ KotlinExpressions.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+
+    val a = 12
+    val b = 13
+    val max: Int
+
+    max = if (a > b) a else b
+    println("$max")
+
+    val flag = true
+
+    if (flag == true) {      // start of if block
+        print("Hey ")
+        print("jude!")
+    }
+}
